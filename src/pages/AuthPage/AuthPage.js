953150fed51f5bcd8835dab8112e95f5962e806a
“[firstname <clarknaomi7@gmail.com>
2023-05-24 05:45:09 -0500

    added images, still need to finish pages

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,12 +1,25 @@
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm'
 
+const daisyMae = {
+  textAlign: "center",
+  width: "25vw",
+  height: "35vh",
+  padding: "2px",
+  
+
+
+};
+
 export default function AuthPage({ setUser }) {
   return (
     <>
+  <div style={daisyMae}><img src={`daisy-mae.png`}></img></div>
+  
       <h1>AuthPage</h1>
       <SignUpForm setUser={setUser} />
       <LoginForm setUser={setUser} />
+      
     </>
   );
 }
